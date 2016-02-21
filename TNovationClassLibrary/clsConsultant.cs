7d858dbf740764e7113cb24e987dbf061573797e
MwenyaJunior <mwenya@live.co.uk>
2016-02-21 18:51:29 +0000

    Added files via upload

---
+++ TNovationClassLibrary/clsConsultant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsultantClassLibrary
+{
+    public class clsConsultant
+    {
+        public string Address { get; set; }
+
+        public int ConsultantNo { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string EmergencyContact { get; set; }
+
+        public DateTime EmploymentDate { get; set; }
+
+        public int HoursOfWork { get; set; }
+
+        public string JobDescription { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Status { get; set; }
+
+        public string TelephoneNo { get; set; }
+
+        public string WorkHistory { get; set; }
+    }
+}
