7fa7a0599f9fde18d615b2d0d33dd142b785f8ce
VijayModhwadia <p13239500@myemail.dmu.ac.uk>
2016-02-22 17:45:06 +0000

    created clsCompany class and properties

---
+++ TNovationClassLibrary/clsCompany.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TNovationClassLibrary
+{
+    public class clsCompany
+    {
+       
+
+        public int CompanyCode { get; set; }
+
+        public string CompanyAddress { get; set; }
+
+        public string CompanyEmailAddress { get; set; }
+
+        public string InvolvedClient { get; set; }
+
+        public string InvolvedProject { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ TNovationTestFramework/tstCompany.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TNovationClassLibrary;
+
+namespace TNovationTestFramework
+{
+    [TestClass]
+    public class tstCompany
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany ACompany = new clsCompany();
+            //test to see that it exists
+            Assert.IsNotNull(ACompany);
+        }
+
+
+        [TestMethod]
+        public void CompanyAddressOK()
+        {
+            //create an instance of the class you want to create
+            clsCompany ACompany = new clsCompany();
+            //create to some test data to assign to the property
+            string SomeCompany = "TNovation";
+            //assign the data to the property
+            ACompany.CompanyAddress = SomeCompany;
+            //test to see the two values are same
+            Assert.AreEqual(ACompany.CompanyAddress, SomeCompany);
+        }
+        [TestMethod]
+        public void CompanyCodeOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany ACode = new clsCompany();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            ACode.CompanyCode = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(ACode.CompanyCode, TestData);
+        }
+        [TestMethod]
+        public void CompanyEmailAddressOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany AnEmail = new clsCompany();
+            //create some test data to assign to the property
+            string TestData = "vijaymodhwadia@hotmail.co.uk";
+            //assign the data to the property
+            AnEmail.CompanyEmailAddress = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AnEmail.CompanyEmailAddress, TestData);
+        }
+        [TestMethod]
+        public void InvolvedClientOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany AClient = new clsCompany();
+            //create some test data to assign to the property
+            string ATestData = "Freddy";
+            //assign the data to the property
+            AClient.InvolvedClient = ATestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AClient.InvolvedClient, ATestData);
+        }
+        [TestMethod]
+        public void InvolvedProjectOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany AProject = new clsCompany();
+            //create some test data to assign to the property
+            string TestData = "MyProject";
+            //assign the data to the property
+            AProject.InvolvedProject = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AProject.InvolvedProject, TestData);
+        }
+        [TestMethod]
+        public void MobileNumberOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany AMobile = new clsCompany();
+            //create some test data to assign to the property
+            string TestData = "676767";
+            //assign the data to the property
+            AMobile.MobileNumber = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(AMobile.MobileNumber, TestData);
+        }
+
+        [TestMethod]
+        public void PhoneNumberOK()
+        {
+            //create an instance of the class we want to create
+            clsCompany APhone = new clsCompany();
+            //create some test data to assign to the property
+            string TestData = "3345354";
+            //assign the data to the property
+            APhone.PhoneNumber = TestData;
+            //test to see the two values are the same
+            Assert.AreEqual(APhone.PhoneNumber, TestData);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
