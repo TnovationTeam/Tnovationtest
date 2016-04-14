cf9857d3f66f0ba13a8759371db1d993186ab995
Freddy Jilson <freddyjilson@hotmail.com>
2016-04-14 22:59:58 +0100

    Created ClientList form and ADD method

---
+++ TNovationProject/ClientList.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TNovationProject
+{
+    partial class ClientList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxClient = new System.Windows.Forms.ListBox();
+            this.labelClientInformation = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listboxClient
+            // 
+            this.listboxClient.FormattingEnabled = true;
+            this.listboxClient.Location = new System.Drawing.Point(176, 70);
+            this.listboxClient.Name = "listboxClient";
+            this.listboxClient.Size = new System.Drawing.Size(361, 147);
+            this.listboxClient.TabIndex = 60;
+            // 
+            // labelClientInformation
+            // 
+            this.labelClientInformation.AutoSize = true;
+            this.labelClientInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClientInformation.Location = new System.Drawing.Point(36, 30);
+            this.labelClientInformation.Name = "labelClientInformation";
+            this.labelClientInformation.Size = new System.Drawing.Size(181, 20);
+            this.labelClientInformation.TabIndex = 59;
+            this.labelClientInformation.Text = "List client information";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(471, 314);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(86, 23);
+            this.buttonDelete.TabIndex = 58;
+            this.buttonDelete.Text = "Delete Client";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(260, 314);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(86, 23);
+            this.buttonUpdate.TabIndex = 57;
+            this.buttonUpdate.Text = "Update Client";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(83, 314);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(86, 23);
+            this.buttonAdd.TabIndex = 56;
+            this.buttonAdd.Text = "Add Client";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // ClientList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 419);
+            this.Controls.Add(this.listboxClient);
+            this.Controls.Add(this.labelClientInformation);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "ClientList";
+            this.Text = "ClientList";
+            this.Load += new System.EventHandler(this.ClientList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxClient;
+        private System.Windows.Forms.Label labelClientInformation;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
---
+++ TNovationProject/ClientList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TNovationProject
+{
+    public partial class ClientList : Form
+    {
+        public ClientList()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //This line of code navigates to the Add Client page when clicked on Add client button.
+            new AddClient().Show();
+            this.Hide();
+            
+        }
+    }
+}
