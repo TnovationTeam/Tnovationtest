5bca91ae0ca79d953cee482d36dc01f22f518bd6
mwenyajunior <mwenya@live.co.uk>
2016-04-14 13:41:27 +0100

    Updated db

--- TNovationClassLibrary/clsConsultant.cs
+++ TNovationClassLibrary/clsConsultant.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TNovationClassLibrary;
 
-namespace ConsultantClassLibrary
+namespace TNovationClassLibrary
+
 {
     public class clsConsultant
     {
