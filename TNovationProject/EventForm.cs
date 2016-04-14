0486b81deef0581e77b14454b47983c06c5ac129
EgleSc <egle.sci12@yahoo.co.uk>
2016-04-14 12:55:33 +0100

    forms added

---
+++ TNovationProject/EventForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TNovationProject
+{
+    public partial class EventForm : Form
+    {
+        public EventForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
