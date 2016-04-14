20671fa033eea9c165c4301d9fe91cd04e7a57fd
Freddy Jilson <freddyjilson@hotmail.com>
2016-04-14 20:03:30 +0100

    Completed properties for clsClientCollection

--- TNovationClassLibrary/clsClientCollection.cs
+++ TNovationClassLibrary/clsClientCollection.cs
@@ -7,8 +7,81 @@ namespace TNovationClassLibrary
 {
     public class clsClientCollection
     {
-        public List<clsClient> ClientList { get; set; }
+        //private data member for the list
+        List<clsClient> clientList = new List<clsClient>();
 
-        public object Count { get; set; }
+
+        //public property for the client list
+        public List<clsClient> ClientList 
+        {
+
+            get 
+            {
+               //return the private data 
+                  return clientList;
+            }
+            
+           set
+            {
+               //set the private data
+                clientList = value;
+
+            }
+        
+        }
+
+        //public property for count
+        public int Count
+        {
+            get
+            
+            {
+            //return the count of the list 
+                return clientList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        
+        public clsClient ThisClient { get; set; }
+        
+        //public constructor for the class
+        public clsClientCollection()
+        {
+            //var for the index 
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblClient_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsClient AClient = new clsClient();
+                //read in the fields from the current record
+                AClient.ClientNo = Convert.ToInt32(DB.DataTable.Rows[Index]["ClientNo"]);
+                AClient.ClientName = Convert.ToString(DB.DataTable.Rows[Index]["ClientName"]);
+                AClient.ClientEmail = Convert.ToString(DB.DataTable.Rows[Index]["ClientEmail"]);
+                AClient.ClientPosition = Convert.ToString(DB.DataTable.Rows[Index]["ClientPosition"]);
+                AClient.ClientQualification = Convert.ToString(DB.DataTable.Rows[Index]["ClientQualification"]);
+                AClient.ClientService = Convert.ToString(DB.DataTable.Rows[Index]["ClientService"]);
+                AClient.ClientAddress = Convert.ToString(DB.DataTable.Rows[Index]["ClientAddress"]);
+                AClient.ClientTel = Convert.ToInt32(DB.DataTable.Rows[Index]["ClientTel"]);
+                AClient.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                AClient.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                //add the record to the private data member
+                clientList.Add(AClient);
+                //point at the next record
+                Index++;
+            }
+            
+        }
     }
 }
--- TNovationTestFramework/tstClientCollection.cs
+++ TNovationTestFramework/tstClientCollection.cs
@@ -51,25 +51,67 @@ namespace TNovationTestFramework
            
         }
 
+        
+
+
         [TestMethod]
-        public void CountPropertyOK()
+        public void ThisClientPropertyOK()
         {
             //create an instance of the class we want to create 
             clsClientCollection AllClients = new clsClientCollection();
             //create some test data to assign to the property
-            Int32 SomeCount = 0;
-            //assignt the data to the property
-            AllClients.Count = SomeCount;
+            clsClient TestClient = new clsClient();
+            //set the properties of the test object
+            TestClient.Active = true;
+            TestClient.ClientAddress = "21b";
+            TestClient.ClientEmail = "freddy@gmail.com";
+            TestClient.ClientName = "freddy";
+            TestClient.ClientNo = 21;
+            TestClient.ClientPosition = "CEO";
+            TestClient.ClientQualification = "degree";
+            TestClient.ClientService = "IT support for company";
+            TestClient.ClientTel = 783763;
+            TestClient.DateAdded = DateTime.Now.Date;
+            //assign the data to the property
+            AllClients.ThisClient = TestClient;
             //test to see that the two values are the same
-            Assert.AreEqual(AllClients.Count, SomeCount);
-            
+            Assert.AreEqual(AllClients.ThisClient, TestClient);
+
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create 
+            clsClientCollection AllClients = new clsClientCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects 
+            List<clsClient> TestList = new List<clsClient>();
+            //add an item to the list
+            //create the item of the test data
+            clsClient TestItem = new clsClient();
+            //set its properties
+            TestItem.Active = true;
+            TestItem.ClientAddress = "21b";
+            TestItem.ClientEmail = "freddy@gmail.com";
+            TestItem.ClientName = "freddy";
+            TestItem.ClientNo = 21;
+            TestItem.ClientPosition = "CEO";
+            TestItem.ClientQualification = "degree";
+            TestItem.ClientService = "IT support for company";
+            TestItem.ClientTel = 783763;
+            TestItem.DateAdded = DateTime.Now.Date;
+            //add the item to the item test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllClients.ClientList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllClients.Count, TestList.Count);
+
         }
 
 
-
-
-
-
+        
 
 
 
