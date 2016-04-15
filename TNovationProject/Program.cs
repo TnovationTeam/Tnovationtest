5634adc2d864a19bb32d5a2dc12fc3ca19fe3948
Freddy Jilson <freddyjilson@hotmail.com>
2016-04-15 15:49:12 +0100

    Added a MDI parent form and programme.cs(Dont delete the program.cs class)

---
+++ TNovationProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TNovationProject
+{//////////////////////////////// TEAM MEMBERS PLEASE DONT DELETE THIS CLASS FILE    ////////////////////////
+    static class Program
+    {
+
+        // the main entry point of the application
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new mdiBackend());
+        }
+    }
+}
