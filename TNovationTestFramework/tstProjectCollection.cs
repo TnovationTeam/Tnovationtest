16511e7b5d3293f725207d251be184533c1a18e4
Carvalth <callumvarle@gmail.com>
2016-04-22 13:18:04 +0100

    Updated tests

--- TNovationTestFramework/tstProjectCollection.cs
+++ TNovationTestFramework/tstProjectCollection.cs
@@ -24,7 +24,7 @@ namespace TNovationTestFramework
             //create an instance of the class we want to create
             clsProjectCollection ProjectList = new clsProjectCollection();
             //create some test dat to ssign to the property
-            Int32 SomeCount = 2;
+            Int32 SomeCount = 1;
             //assign the data to the property
             ProjectList.Count = SomeCount;
             //test to see that the two values are the same
