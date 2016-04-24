d9b4e1ea6412a4232aa2ef5f52dff354b7be5704
EgleSc <Midori/17>
2016-04-24 21:53:01 +0100

    updated

--- TNovationProject/ListEvent.Designer.cs
+++ TNovationProject/ListEvent.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxEvent = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxEvent
+            // 
+            this.listBoxEvent.FormattingEnabled = true;
+            this.listBoxEvent.Location = new System.Drawing.Point(26, 75);
+            this.listBoxEvent.Name = "listBoxEvent";
+            this.listBoxEvent.Size = new System.Drawing.Size(168, 225);
+            this.listBoxEvent.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(437, 75);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(437, 146);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
             // 
             // ListEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 361);
+            this.ClientSize = new System.Drawing.Size(558, 427);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxEvent);
             this.Name = "ListEvent";
             this.Text = "ListEvent";
             this.ResumeLayout(false);
@@ -42,5 +74,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxEvent;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
     }
 }
