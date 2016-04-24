c3570f6686e3aa4d58d0874b5c34f57e91b10dd2
EgleSc <Midori/17>
2016-04-24 19:46:06 +0100

    new forms

--- TNovationProject/EventForm.cs
+++ TNovationProject/EventForm.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using TNovationClassLibrary;
 
 namespace TNovationProject
 {
@@ -22,75 +21,5 @@ namespace TNovationProject
         {
 
         }
-        void Add()
-        {
-            //create an instance of TNovation
-            clsEventCollection TNovation = new clsEventCollection();
-            //validate the data on the webform
-            Boolean OK = TNovation.ThisEvent.Valid(textBoxEventName.Text, textBoxCompanyName.Text, textBoxConsultantAttending.Text, textBoxContact.Text, textBoxStartDate.Text, textBoxGuestSpeaker.Text, textBoxLocation.Text, textBoxTypeOfEvent.Text);
-            //if the data is OK then add it to the object
-            if (OK == true)
-            {
-                //get the data entered by the user
-                TNovation.ThisEvent.EventCode = Convert.ToInt32(textBoxEventCode.Text);
-                TNovation.ThisEvent.EventName = textBoxEventName.Text;
-                TNovation.ThisEvent.CompanyName = textBoxCompanyName.Text;
-                TNovation.ThisEvent.ConsultantAttending = textBoxConsultantAttending.Text;
-                TNovation.ThisEvent.Contact = textBoxContact.Text;
-                TNovation.ThisEvent.StartDate = Convert.ToDateTime(textBoxStartDate.Text);
-                TNovation.ThisEvent.GuestSpeaker = textBoxGuestSpeaker.Text;
-                TNovation.ThisEvent.Location = textBoxLocation.Text;
-                TNovation.ThisEvent.TypeOfEvent = textBoxTypeOfEvent.Text;
-
-                //add the record
-                TNovation.Add();
-            }
-            else
-            {
-                //report an error 
-                labelError.Text = "There were problems with the data entered";
-            }
-        }
-       
-
-        private void buttonBookIn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonSave_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonBack_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonAdd_Click(object sender, EventArgs e)
-        {
-            //add new record
-            Add();
-      
-            new EventList().Show();
-            this.Hide();
-        }
-
-        private void textBoxEventCode_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonUpdate_Click(object sender, EventArgs e)
-        {
-            
-            
-           
-            new EventList().Show();
-            this.Hide();
-        }
-
-       
     }
 }
