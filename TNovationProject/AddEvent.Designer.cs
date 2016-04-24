e63893e3ea63c1acebb19ef9d95569cf21f29358
EgleSc <Midori/17>
2016-04-24 21:05:54 +0100

    redone forms

---
+++ TNovationProject/AddEvent.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace TNovationProject
+{
+    partial class AddEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelEventCode = new System.Windows.Forms.Label();
+            this.labelEventName = new System.Windows.Forms.Label();
+            this.labelCompanyName = new System.Windows.Forms.Label();
+            this.labelConsultantAttending = new System.Windows.Forms.Label();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.labelGuestSpeaker = new System.Windows.Forms.Label();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.labelTypeOfEvent = new System.Windows.Forms.Label();
+            this.textBoxEventCode = new System.Windows.Forms.TextBox();
+            this.textBoxEventName = new System.Windows.Forms.TextBox();
+            this.textBoxCompanyName = new System.Windows.Forms.TextBox();
+            this.textBoxConsultantAttending = new System.Windows.Forms.TextBox();
+            this.textBoxContact = new System.Windows.Forms.TextBox();
+            this.textBoxStartDate = new System.Windows.Forms.TextBox();
+            this.textBoxGuestSpeaker = new System.Windows.Forms.TextBox();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.textBoxTypeOfEvent = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.labelHeading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelEventCode
+            // 
+            this.labelEventCode.AutoSize = true;
+            this.labelEventCode.Location = new System.Drawing.Point(32, 156);
+            this.labelEventCode.Name = "labelEventCode";
+            this.labelEventCode.Size = new System.Drawing.Size(66, 13);
+            this.labelEventCode.TabIndex = 0;
+            this.labelEventCode.Text = "Event Code:";
+            // 
+            // labelEventName
+            // 
+            this.labelEventName.AutoSize = true;
+            this.labelEventName.Location = new System.Drawing.Point(29, 205);
+            this.labelEventName.Name = "labelEventName";
+            this.labelEventName.Size = new System.Drawing.Size(69, 13);
+            this.labelEventName.TabIndex = 1;
+            this.labelEventName.Text = "Event Name:";
+            // 
+            // labelCompanyName
+            // 
+            this.labelCompanyName.AutoSize = true;
+            this.labelCompanyName.Location = new System.Drawing.Point(29, 254);
+            this.labelCompanyName.Name = "labelCompanyName";
+            this.labelCompanyName.Size = new System.Drawing.Size(85, 13);
+            this.labelCompanyName.TabIndex = 2;
+            this.labelCompanyName.Text = "Company Name:";
+            // 
+            // labelConsultantAttending
+            // 
+            this.labelConsultantAttending.AutoSize = true;
+            this.labelConsultantAttending.Location = new System.Drawing.Point(32, 308);
+            this.labelConsultantAttending.Name = "labelConsultantAttending";
+            this.labelConsultantAttending.Size = new System.Drawing.Size(108, 13);
+            this.labelConsultantAttending.TabIndex = 3;
+            this.labelConsultantAttending.Text = "Consultant Attending:";
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Location = new System.Drawing.Point(29, 365);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(47, 13);
+            this.labelContact.TabIndex = 4;
+            this.labelContact.Text = "Contact:";
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Location = new System.Drawing.Point(408, 154);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(52, 13);
+            this.labelStartDate.TabIndex = 5;
+            this.labelStartDate.Text = "StartDate";
+            // 
+            // labelGuestSpeaker
+            // 
+            this.labelGuestSpeaker.AutoSize = true;
+            this.labelGuestSpeaker.Location = new System.Drawing.Point(411, 201);
+            this.labelGuestSpeaker.Name = "labelGuestSpeaker";
+            this.labelGuestSpeaker.Size = new System.Drawing.Size(81, 13);
+            this.labelGuestSpeaker.TabIndex = 6;
+            this.labelGuestSpeaker.Text = "Guest Speaker:";
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.AutoSize = true;
+            this.labelLocation.Location = new System.Drawing.Point(408, 254);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(51, 13);
+            this.labelLocation.TabIndex = 7;
+            this.labelLocation.Text = "Location:";
+            // 
+            // labelTypeOfEvent
+            // 
+            this.labelTypeOfEvent.AutoSize = true;
+            this.labelTypeOfEvent.Location = new System.Drawing.Point(408, 308);
+            this.labelTypeOfEvent.Name = "labelTypeOfEvent";
+            this.labelTypeOfEvent.Size = new System.Drawing.Size(79, 13);
+            this.labelTypeOfEvent.TabIndex = 8;
+            this.labelTypeOfEvent.Text = "Type Of Event:";
+            // 
+            // textBoxEventCode
+            // 
+            this.textBoxEventCode.Location = new System.Drawing.Point(119, 156);
+            this.textBoxEventCode.Name = "textBoxEventCode";
+            this.textBoxEventCode.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEventCode.TabIndex = 9;
+            // 
+            // textBoxEventName
+            // 
+            this.textBoxEventName.Location = new System.Drawing.Point(119, 205);
+            this.textBoxEventName.Name = "textBoxEventName";
+            this.textBoxEventName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEventName.TabIndex = 10;
+            // 
+            // textBoxCompanyName
+            // 
+            this.textBoxCompanyName.Location = new System.Drawing.Point(120, 251);
+            this.textBoxCompanyName.Name = "textBoxCompanyName";
+            this.textBoxCompanyName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCompanyName.TabIndex = 11;
+            // 
+            // textBoxConsultantAttending
+            // 
+            this.textBoxConsultantAttending.Location = new System.Drawing.Point(161, 308);
+            this.textBoxConsultantAttending.Name = "textBoxConsultantAttending";
+            this.textBoxConsultantAttending.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConsultantAttending.TabIndex = 12;
+            // 
+            // textBoxContact
+            // 
+            this.textBoxContact.Location = new System.Drawing.Point(131, 362);
+            this.textBoxContact.Name = "textBoxContact";
+            this.textBoxContact.Size = new System.Drawing.Size(100, 20);
+            this.textBoxContact.TabIndex = 13;
+            // 
+            // textBoxStartDate
+            // 
+            this.textBoxStartDate.Location = new System.Drawing.Point(486, 151);
+            this.textBoxStartDate.Name = "textBoxStartDate";
+            this.textBoxStartDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStartDate.TabIndex = 14;
+            // 
+            // textBoxGuestSpeaker
+            // 
+            this.textBoxGuestSpeaker.Location = new System.Drawing.Point(512, 194);
+            this.textBoxGuestSpeaker.Name = "textBoxGuestSpeaker";
+            this.textBoxGuestSpeaker.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGuestSpeaker.TabIndex = 15;
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(465, 251);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLocation.TabIndex = 16;
+            // 
+            // textBoxTypeOfEvent
+            // 
+            this.textBoxTypeOfEvent.Location = new System.Drawing.Point(493, 305);
+            this.textBoxTypeOfEvent.Name = "textBoxTypeOfEvent";
+            this.textBoxTypeOfEvent.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTypeOfEvent.TabIndex = 17;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(537, 420);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 18;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(703, 420);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 19;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // labelHeading
+            // 
+            this.labelHeading.AutoSize = true;
+            this.labelHeading.Font = new System.Drawing.Font("Microsoft YaHei", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeading.Location = new System.Drawing.Point(26, 18);
+            this.labelHeading.Name = "labelHeading";
+            this.labelHeading.Size = new System.Drawing.Size(244, 50);
+            this.labelHeading.TabIndex = 20;
+            this.labelHeading.Text = "T{Novation}";
+            // 
+            // AddEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 489);
+            this.Controls.Add(this.labelHeading);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTypeOfEvent);
+            this.Controls.Add(this.textBoxLocation);
+            this.Controls.Add(this.textBoxGuestSpeaker);
+            this.Controls.Add(this.textBoxStartDate);
+            this.Controls.Add(this.textBoxContact);
+            this.Controls.Add(this.textBoxConsultantAttending);
+            this.Controls.Add(this.textBoxCompanyName);
+            this.Controls.Add(this.textBoxEventName);
+            this.Controls.Add(this.textBoxEventCode);
+            this.Controls.Add(this.labelTypeOfEvent);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.labelGuestSpeaker);
+            this.Controls.Add(this.labelStartDate);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.labelConsultantAttending);
+            this.Controls.Add(this.labelCompanyName);
+            this.Controls.Add(this.labelEventName);
+            this.Controls.Add(this.labelEventCode);
+            this.Name = "AddEvent";
+            this.Text = "AddEvent";
+            this.Load += new System.EventHandler(this.AddEvent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelEventCode;
+        private System.Windows.Forms.Label labelEventName;
+        private System.Windows.Forms.Label labelCompanyName;
+        private System.Windows.Forms.Label labelConsultantAttending;
+        private System.Windows.Forms.Label labelContact;
+        private System.Windows.Forms.Label labelStartDate;
+        private System.Windows.Forms.Label labelGuestSpeaker;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.Label labelTypeOfEvent;
+        private System.Windows.Forms.TextBox textBoxEventCode;
+        private System.Windows.Forms.TextBox textBoxEventName;
+        private System.Windows.Forms.TextBox textBoxCompanyName;
+        private System.Windows.Forms.TextBox textBoxConsultantAttending;
+        private System.Windows.Forms.TextBox textBoxContact;
+        private System.Windows.Forms.TextBox textBoxStartDate;
+        private System.Windows.Forms.TextBox textBoxGuestSpeaker;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.TextBox textBoxTypeOfEvent;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Label labelHeading;
+    }
+}
--- TNovationProject/AddEvent.cs
+++ TNovationProject/AddEvent.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TNovationClassLibrary;
 
 namespace TNovationProject
 {
@@ -15,6 +16,61 @@ namespace TNovationProject
         public AddEvent()
         {
             InitializeComponent();
+        }
+
+        private void AddEvent_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //add new record
+            Add();
+          // lblError.Text = "The data record is entered to the database! Click back to return to the clients list page.";
+            //This line of code navigates to the Client list page when clicked on add button.
+            new EventList().Show();
+            this.Hide();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            //This line of code navigates to the Client list page when clicked on cancel button.
+            new EventList().Show();
+            this.Hide();
+        }
+
+        void Add()
+        {
+            //create an instance of TNovation
+            clsEventCollection TNovation = new clsEventCollection();
+            //validate the data on the webform
+            Boolean OK = TNovation.ThisEvent.Valid(textBoxEventName.Text, textBoxCompanyName.Text, textBoxConsultantAttending.Text, textBoxContact.Text, textBoxStartDate.Text, textBoxGuestSpeaker.Text, textBoxLocation.Text, textBoxTypeOfEvent.Text);
+            //if the data is OK then add it to the object
+            if (OK == true)
+            {
+                //get the data entered by the user
+                TNovation.ThisEvent.EventCode = Convert.ToInt32(textBoxEventCode.Text);
+                TNovation.ThisEvent.EventName = textBoxEventName.Text;
+                TNovation.ThisEvent.CompanyName = textBoxCompanyName.Text;
+                TNovation.ThisEvent.ConsultantAttending = textBoxConsultantAttending.Text;
+                TNovation.ThisEvent.Contact = textBoxContact.Text;
+                TNovation.ThisEvent.StartDate = Convert.ToDateTime(textBoxStartDate.Text);
+                TNovation.ThisEvent.GuestSpeaker = textBoxGuestSpeaker.Text;
+                TNovation.ThisEvent.Location = textBoxLocation.Text;
+                TNovation.ThisEvent.TypeOfEvent = textBoxTypeOfEvent.Text;
+                //add the record
+                TNovation.Add();
+            }
+            else
+            {
+                //report an error 
+               // lblError.Text = "There were problems with the data entered";
+            }
+
+
+
+
         }
     }
 }
