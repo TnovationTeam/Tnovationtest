349709021b3f86c5e5ddf1e2958e3eb5d7fb361f
VijayModhwadia <p13239500@myemail.dmu.ac.uk>
2016-04-25 11:25:12 +0100

    I have made an extra form called UpdateCompany

--- TNovationProject/UpdateCompany.Designer.cs
+++ TNovationProject/UpdateCompany.Designer.cs
@@ -31,6 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateCompany));
             this.labelHeading = new System.Windows.Forms.Label();
             this.labelLogo = new System.Windows.Forms.PictureBox();
+            this.labelInstruction1 = new System.Windows.Forms.Label();
+            this.labelInstruction2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPhoneNumber = new System.Windows.Forms.Label();
+            this.labelMobileNumber = new System.Windows.Forms.Label();
+            this.labelInvolvedProject = new System.Windows.Forms.Label();
+            this.labelInvolvedClient = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCompanyAddress = new System.Windows.Forms.Label();
+            this.labelCompanyCode = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxCompanyCode = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.labelLogo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +57,7 @@
             this.labelHeading.BackColor = System.Drawing.Color.LightSteelBlue;
             this.labelHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelHeading.ForeColor = System.Drawing.Color.Navy;
-            this.labelHeading.Location = new System.Drawing.Point(326, 70);
+            this.labelHeading.Location = new System.Drawing.Point(361, 32);
             this.labelHeading.Name = "labelHeading";
             this.labelHeading.Size = new System.Drawing.Size(334, 73);
             this.labelHeading.TabIndex = 27;
@@ -49,18 +66,175 @@
             // labelLogo
             // 
             this.labelLogo.Image = ((System.Drawing.Image)(resources.GetObject("labelLogo.Image")));
-            this.labelLogo.Location = new System.Drawing.Point(65, 70);
+            this.labelLogo.Location = new System.Drawing.Point(100, 32);
             this.labelLogo.Name = "labelLogo";
             this.labelLogo.Size = new System.Drawing.Size(164, 74);
             this.labelLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.labelLogo.TabIndex = 28;
             this.labelLogo.TabStop = false;
             // 
+            // labelInstruction1
+            // 
+            this.labelInstruction1.AutoSize = true;
+            this.labelInstruction1.Location = new System.Drawing.Point(142, 173);
+            this.labelInstruction1.Name = "labelInstruction1";
+            this.labelInstruction1.Size = new System.Drawing.Size(613, 13);
+            this.labelInstruction1.TabIndex = 29;
+            this.labelInstruction1.Text = "To edit a record, please find the company code in the company list page and then " +
+    "enter the data fields here in the textbox to edit.";
+            // 
+            // labelInstruction2
+            // 
+            this.labelInstruction2.AutoSize = true;
+            this.labelInstruction2.Location = new System.Drawing.Point(142, 199);
+            this.labelInstruction2.Name = "labelInstruction2";
+            this.labelInstruction2.Size = new System.Drawing.Size(289, 13);
+            this.labelInstruction2.TabIndex = 30;
+            this.labelInstruction2.Text = "*Client Number can be found in the list box of client list page";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Edit company information";
+            // 
+            // labelPhoneNumber
+            // 
+            this.labelPhoneNumber.AutoSize = true;
+            this.labelPhoneNumber.Location = new System.Drawing.Point(350, 355);
+            this.labelPhoneNumber.Name = "labelPhoneNumber";
+            this.labelPhoneNumber.Size = new System.Drawing.Size(78, 13);
+            this.labelPhoneNumber.TabIndex = 83;
+            this.labelPhoneNumber.Text = "Phone Number";
+            // 
+            // labelMobileNumber
+            // 
+            this.labelMobileNumber.AutoSize = true;
+            this.labelMobileNumber.Location = new System.Drawing.Point(350, 317);
+            this.labelMobileNumber.Name = "labelMobileNumber";
+            this.labelMobileNumber.Size = new System.Drawing.Size(78, 13);
+            this.labelMobileNumber.TabIndex = 82;
+            this.labelMobileNumber.Text = "Mobile Number";
+            // 
+            // labelInvolvedProject
+            // 
+            this.labelInvolvedProject.AutoSize = true;
+            this.labelInvolvedProject.Location = new System.Drawing.Point(344, 281);
+            this.labelInvolvedProject.Name = "labelInvolvedProject";
+            this.labelInvolvedProject.Size = new System.Drawing.Size(84, 13);
+            this.labelInvolvedProject.TabIndex = 81;
+            this.labelInvolvedProject.Text = "Involved Project";
+            // 
+            // labelInvolvedClient
+            // 
+            this.labelInvolvedClient.AutoSize = true;
+            this.labelInvolvedClient.Location = new System.Drawing.Point(81, 394);
+            this.labelInvolvedClient.Name = "labelInvolvedClient";
+            this.labelInvolvedClient.Size = new System.Drawing.Size(77, 13);
+            this.labelInvolvedClient.TabIndex = 80;
+            this.labelInvolvedClient.Text = "Involved Client";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 79;
+            this.label3.Text = "Company Email Address";
+            // 
+            // labelCompanyAddress
+            // 
+            this.labelCompanyAddress.AutoSize = true;
+            this.labelCompanyAddress.Location = new System.Drawing.Point(66, 314);
+            this.labelCompanyAddress.Name = "labelCompanyAddress";
+            this.labelCompanyAddress.Size = new System.Drawing.Size(92, 13);
+            this.labelCompanyAddress.TabIndex = 78;
+            this.labelCompanyAddress.Text = "Company Address";
+            // 
+            // labelCompanyCode
+            // 
+            this.labelCompanyCode.AutoSize = true;
+            this.labelCompanyCode.Location = new System.Drawing.Point(79, 281);
+            this.labelCompanyCode.Name = "labelCompanyCode";
+            this.labelCompanyCode.Size = new System.Drawing.Size(79, 13);
+            this.labelCompanyCode.TabIndex = 77;
+            this.labelCompanyCode.Text = "Company Code";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(434, 352);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 76;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(434, 314);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 75;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(434, 278);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 74;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 391);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 73;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 352);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 72;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 314);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 71;
+            // 
+            // textBoxCompanyCode
+            // 
+            this.textBoxCompanyCode.Location = new System.Drawing.Point(164, 278);
+            this.textBoxCompanyCode.Name = "textBoxCompanyCode";
+            this.textBoxCompanyCode.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCompanyCode.TabIndex = 70;
+            // 
             // UpdateCompany
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1001, 597);
+            this.Controls.Add(this.labelPhoneNumber);
+            this.Controls.Add(this.labelMobileNumber);
+            this.Controls.Add(this.labelInvolvedProject);
+            this.Controls.Add(this.labelInvolvedClient);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCompanyAddress);
+            this.Controls.Add(this.labelCompanyCode);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxCompanyCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelInstruction2);
+            this.Controls.Add(this.labelInstruction1);
             this.Controls.Add(this.labelLogo);
             this.Controls.Add(this.labelHeading);
             this.Name = "UpdateCompany";
@@ -75,5 +249,22 @@
 
         private System.Windows.Forms.Label labelHeading;
         private System.Windows.Forms.PictureBox labelLogo;
+        private System.Windows.Forms.Label labelInstruction1;
+        private System.Windows.Forms.Label labelInstruction2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPhoneNumber;
+        private System.Windows.Forms.Label labelMobileNumber;
+        private System.Windows.Forms.Label labelInvolvedProject;
+        private System.Windows.Forms.Label labelInvolvedClient;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCompanyAddress;
+        private System.Windows.Forms.Label labelCompanyCode;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxCompanyCode;
     }
 }
